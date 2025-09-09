5a13e056cacacaf1ca6578856df1c3f65153aa41
Antoino Villada <antonio.viillada.7@gmail.com>
2025-09-09 16:51:50 -0500

    configuracion de pom

---
+++ citas-service/src/main/java/medisystem/avanzada/uq/citas_service/controller/MiController.java
@@ -0,0 +1,16 @@
+package medisystem.avanzada.uq.citas_service.controller;
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class MiController {
+
+
+    @GetMapping("/")
+    public String pruebas(){
+
+        return "index";
+    }
+}
